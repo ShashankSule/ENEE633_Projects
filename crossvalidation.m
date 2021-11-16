e777b22df0808bf868fbfed4dfd96280882c662a
Shashank Sule <shashanksule8@gmail.com>
2021-11-16 00:20:10 -0500

    Cross val data

--- crossvalidation.m
+++ crossvalidation.m
@@ -2,13 +2,24 @@
 
 %% set up data
 
-[~,training, y] = loadandfiddle();
-sigma_params = [1; 1e1; 1e2; 1e3; 1e4]; 
+[training,~, y] = loadandfiddle();
+sigma_params = [1;10;20;30;40;50;60;70;80;90;100;110;120;130;140;150]; 
+err = zeros(length(sigma_params),1);
 %% compute cross vals! 
-for i = 1:5
+for i = 1:length(sigma_params)
+Sigma = sigma_params(i);
 fprintf("Sigma = %d \n", sigma_params(i)); 
 sigma = sigma_params(i); % variance parameter 
 k = @(x,y) exp(-(norm(x-y)^2)/(2*sigma));
-err = crossval('mcr', training, y, 'Predfun',...
-                @(xtrain, ytrain, xtest)svmkernel(xtrain, ytrain, xtest, k));
+err(i) = crossval('mcr', training, y, 'Predfun',...
+                @(xtrain, ytrain, xtest)svmkernel(xtrain, ytrain, xtest, k), ...
+                'KFold', 5);
 end
+
+%% plot the error data
+plot(sigma_params, 100*param_err, 'bo-'); 
+xlabel("$\sigma$", 'Interpreter', 'latex');
+xticks(sigma_params);
+ylabel("Average cross-validation percentage error ", 'Interpreter', 'latex');
+title("Error statistics for tuning $\sigma$ in the RBF kernel via 5-fold cross-validation", 'Interpreter', 'latex');
+set(gca, 'FontSize', 16);
